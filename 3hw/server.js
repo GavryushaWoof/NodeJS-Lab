b1d86a3a0e4f519c174da6cc30b37aaa05dde06c
GavryushaWoof <gavgav.cat@yandex.ru>
2021-01-18 18:37:18 +0300

    fixed hw3

--- 3hw/server.js
+++ 3hw/server.js
@@ -6,16 +6,16 @@ const info = 'info.json';
 
 app.use((request, response, next) => {
     if (request.headers.iknowyoursecret !== 'TheOwlsAreNotWhatTheySeem') {
-        return response.end('Who are the owls?');
+        return response.end('You do not know the secret');
     }
     next();
 });
 
-app.post("/", (request, response) => {
+app.post('/', (request, response) => {
     let logs = JSON.parse(fs.readFileSync(path.resolve(__dirname, info), 'utf-8'));
     const name = request.headers.name;
     if (!name) {
-        return response.end("Wrong name");
+        return response.end('Who are you?');
     }
     logs.push({ name: name, ip: request.connection.remoteAddress });
     fs.writeFile(path.resolve(__dirname, info), JSON.stringify(logs), (err) => {
