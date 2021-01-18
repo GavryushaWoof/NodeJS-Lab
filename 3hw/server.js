a61129721449bb1ce39ae206c7b0ee9234c5d809
GavryushaWoof <gavgav.cat@yandex.ru>
2021-01-18 18:22:13 +0300

    HW3

---
+++ 3hw/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const info = 'info.json';
+
+app.use((request, response, next) => {
+    if (request.headers.iknowyoursecret !== 'TheOwlsAreNotWhatTheySeem') {
+        return response.end('Who are the owls?');
+    }
+    next();
+});
+
+app.post("/", (request, response) => {
+    let logs = JSON.parse(fs.readFileSync(path.resolve(__dirname, info), 'utf-8'));
+    const name = request.headers.name;
+    if (!name) {
+        return response.end("Wrong name");
+    }
+    logs.push({ name: name, ip: request.connection.remoteAddress });
+    fs.writeFile(path.resolve(__dirname, info), JSON.stringify(logs), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    response.send(`Hello, ${request.headers.name}!`);
+    return response.end();
+});
+
+app.listen(8080, (err) => {
+    if (err) {
+        return console.log('An error has occurred', err);
+    }
+});
